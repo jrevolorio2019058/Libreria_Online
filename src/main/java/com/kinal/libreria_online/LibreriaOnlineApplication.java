a65607bac4a132a349c574bf3455ff9a76b00b35
jrevolorio2019058 <jrevolorio-2019058@kinal.edu.gt>
2024-10-21 19:15:15 -0600

    Inicializando Proyecto

---
+++ src/main/java/com/kinal/libreria_online/LibreriaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.kinal.libreria_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaOnlineApplication.class, args);
+	}
+
+}
